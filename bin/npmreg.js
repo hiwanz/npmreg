7592c21ef2a270038a3a3d3bc5f743c1d48d76e7
hiwanz <princeb4d@gmail.com>
2021-09-26 15:46:08 +0800

    style: code style

--- bin/npmreg.js
+++ bin/npmreg.js
@@ -5,6 +5,47 @@
 		exec = require('child_process').exec,
 		configFile = path.join(__dirname, '../config.json')
 
+	function lsConfig() {
+		let config = JSON.parse(fs.readFileSync(configFile, 'utf-8'))
+		let configList = ''
+		for (let item in config) {
+			configList += (item + '	' + config[item] + '\n')
+		}
+		console.log(configList)
+	}
+	
+	function addConfig() {
+		let config = JSON.parse(fs.readFileSync(configFile, 'utf-8'))
+		console.log('Adding', process.argv[3] + '(' + process.argv[4] + ')', 'to config...')
+		config[process.argv[3]] = process.argv[4]
+
+		fs.writeFile(configFile, JSON.stringify(config, null, 4), function (e) {
+			console.log('Done!')
+			if (e) { throw e }
+		})
+	}
+
+	function rmConfig() {
+		let config = JSON.parse(fs.readFileSync(configFile, 'utf-8'))
+		console.log('Removing', process.argv[3], 'from config...')
+		delete config[process.argv[3]]
+		fs.writeFile(configFile, JSON.stringify(config, null, 4), function (e) {
+			console.log('Done!')
+			if (e) { throw e }
+		})
+	}
+
+	function sayHello() {
+		let usageString = 'Usage : npmreg taobao\n' +
+			'will set npm registry to http://registry.npm.taobao.org/\n' +
+			'visit https://github.com/hiwanz/npmreg for more help.\n\n' +
+			'Available registry:\n'
+
+		console.log(usageString)
+		lsConfig()
+		process.exit()
+	}
+
 	function runApp() {
 		let config = JSON.parse(fs.readFileSync(configFile, 'utf-8'))
 		if (process.argv.length == 2) {
@@ -30,43 +71,6 @@
 		}
 	}
 
-	function sayHello() {
-		let usageString = 'Usage : npmreg taobao\n' +
-			'will set npm registry to http://registry.npm.taobao.org/\n' +
-			'visit https://github.com/hiwanz/npmreg for more help.\n\n' +
-			'Available registry:\n'
-
-		console.log(usageString)
-		lsConfig()
-		process.exit()
-	}
-	function lsConfig() {
-		let config = JSON.parse(fs.readFileSync(configFile, 'utf-8'))
-		let configList = ''
-		for (let item in config) {
-			configList += (item + '	' + config[item] + '\n')
-		}
-		console.log(configList)
-	}
-	function addConfig() {
-		let config = JSON.parse(fs.readFileSync(configFile, 'utf-8'))
-		console.log('Adding', process.argv[3] + '(' + process.argv[4] + ')', 'to config...')
-		config[process.argv[3]] = process.argv[4]
-
-		fs.writeFile(configFile, JSON.stringify(config, null, 4), function (e) {
-			console.log('Done!')
-			if (e) { throw e }
-		})
-	}
-	function rmConfig() {
-		let config = JSON.parse(fs.readFileSync(configFile, 'utf-8'))
-		console.log('Removing', process.argv[3], 'from config...')
-		delete config[process.argv[3]]
-		fs.writeFile(configFile, JSON.stringify(config, null, 4), function (e) {
-			console.log('Done!')
-			if (e) { throw e }
-		})
-	}
 	runApp()
 })()
 
